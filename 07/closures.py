0f3ec22638f5a60565612b3d7f7fce9ec169e67f
Ulises Desentis <ulifly@hotmail.com>
2024-03-19 16:24:37 -0600

    updated

--- 07/closures.py
+++ 07/closures.py
@@ -1,3 +1,6 @@
+"""
+# repaso de scoope
+
 def funcion_principal():
     a = 'a'
     b = 'b'
@@ -12,3 +15,20 @@ def funcion_principal():
     print(c)  #esto dara un error puesto que esta variable esta dentro de la funcion anidada y no en este scoope
 
 funcion_principal()
+"""
+
+#un closure es una funcion que puede generar de forma dinamica a otra funcion 
+#y esta funcion puede acceder a las variables locales aun cuando la pirmera haya finalizado
+
+def saludar(username):
+    mensaje = f'Hola {username}' #esta es una variable local
+
+    def mostrar_mensaje():
+        print(mensaje)
+
+    return mostrar_mensaje
+
+username = 'Ulifly'
+respuesta = saludar(username)
+
+respuesta()
