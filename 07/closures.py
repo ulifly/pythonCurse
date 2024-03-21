040bb91c601ce4624be9e83a9f46e91d71733bf3
Ulifly <ulifly@hotmail.com>
2024-03-20 23:47:19 -0600

    module 2 advance

--- 07/closures.py
+++ 07/closures.py
@@ -1,5 +1,5 @@
 """
-# repaso de scoope
+# repaso de scope
 
 def funcion_principal():
     a = 'a'
@@ -8,27 +8,39 @@ def funcion_principal():
     def funcion_anidada():
         c = 'c'
 
-        print(a) #las variables pueden ser accedidas desde cualquier lugar dentro de la funcion anidada.
-        print(c) #pero no desde fuera de la funcion anidada. esto es por que tiene un scoope superior
+        print(a) #las variables pueden ser accedidas desde cualquier lugar dentro de la función anidada.
+        print(c) #pero no desde fuera de la función anidada. esto es por que tiene un scope superior
         print(b)
     funcion_anidada()
-    print(c)  #esto dara un error puesto que esta variable esta dentro de la funcion anidada y no en este scoope
+    print(c)  #esto dará un error puesto que esta variable esta dentro de la función anidada y no en este scope
 
 funcion_principal()
 """
 
-#un closure es una funcion que puede generar de forma dinamica a otra funcion 
-#y esta funcion puede acceder a las variables locales aun cuando la pirmera haya finalizado
+#un closure es una funcion que puede generar de forma dinámica a otra funcion 
+#y esta funcion puede acceder a las variables locales aun cuando la primera haya finalizado
 
 def saludar(username):
     mensaje = f'Hola {username}' #esta es una variable local
 
     def mostrar_mensaje():
-        print(mensaje)
+        print(mensaje) #esta es una variable local y puede acceder a la variable local de la funcion principal
 
     return mostrar_mensaje
 
 username = 'Ulifly'
 respuesta = saludar(username)
 
+respuesta()  
+username = 'otro usuario (esto nunca se va a mostrar)'
+
 respuesta()
+
+"""
+Un closure se refiere a una función anónima que puede acceder a variables fuera de su ámbito de definición. 
+En otras palabras, permite encapsular datos y lógica en un bloque independiente y reutilizable. 
+Los closures son útiles para crear funciones más dinámicas y adaptables a diferentes contextos.
+
+un closure se crea cuando una función interna accede a variables locales de una función externa. 
+Esto permite que la función interna conserve acceso a esas variables incluso después de que la función externa haya finalizado su ejecución.
+"""
