d4871d5004008b52cc46f424b25d08f86ada75f1
Ulises Desentis <ulifly@hotmail.com>
2024-03-21 18:36:09 -0600

    update decorators

--- 07/decoradores.py
+++ 07/decoradores.py
@@ -1,1 +1,21 @@
-zx
+"""
+en python un decorador se define como una funcion que toma como parametro otra funcion
+y retorna una funcion con un comportamiento diferente 
+Esta nueva función encapsula o envuelve la función original, 
+permitiendo modificar su comportamiento. Para aplicar un decorador a una función, 
+se utiliza la sintaxis @nombre_del_decorador justo encima de la definición de la función.
+"""
+
+def funcion_decorador(funcion_a_decorar):
+    def funcion_interior():
+        print("Este es el cuerpo de la funcion decorada")
+        funcion_a_decorar()
+    return funcion_interior
+
+
+
+@funcion_decorador
+def funcion_a_decorar():
+    print("Esta es la funcion a decorar")
+
+funcion_a_decorar()
